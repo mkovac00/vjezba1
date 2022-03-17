597f42b586d78e05fad30eaf79c1224d29265f52
00 <00>
2022-03-17 17:43:42 +0100

    Add project files.

---
+++ ConsoleAppMario/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleAppMario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
