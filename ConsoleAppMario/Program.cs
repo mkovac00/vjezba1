3bad63a126cf79cf24c87eb2798cdb4b96f86fd0
mkovac00 <mkovac25122512@gmail.com>
2022-03-17 17:56:10 +0100

    First commit

--- ConsoleAppMario/Program.cs
+++ ConsoleAppMario/Program.cs
@@ -7,6 +7,8 @@ namespace ConsoleAppMario
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Dodano nakon kreiranja gita!!!");
+            Console.WriteLine("Poyyy");
         }
     }
 }
